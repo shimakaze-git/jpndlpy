16defd50532532e6745f0cbd9cab36fb61bda988
shimakaze-git <shimakaze.soft+github@googlemail.com>
2019-03-11 00:12:34 +0900

    fix: setup.py license format

--- jpndlpy/__init__.py
+++ jpndlpy/__init__.py
@@ -5,7 +5,7 @@ Created on 2019/03/10
 '''
 
 __version__ = '0.1.0'
-__license__ = 'BSD 2-Clause License'
+__license__ = 'BSD 2-Clause "Simplified" License (BSD-2-Clause)'
 __author__ = 'shimakaze-git'
 __author_email__ = 'shimakaze.soft@gmail.com'
 __url__ = 'https://github.com/shimakaze-git/jpndlpy'
