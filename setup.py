2e18276f77b897cd48b555fa933d9d04d115ab59
shimakaze-git <shimakaze.soft+github@googlemail.com>
2019-03-11 04:25:14 +0900

    fix: fix python version setup.py

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: BSD License',
     ],
 )
